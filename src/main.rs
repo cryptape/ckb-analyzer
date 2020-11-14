afb155c747cc3636b67d624d267d619d06918981
keroro <keroroxx520@gmail.com>
2020-11-14 12:05:11 +0800

    analyze uncles

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,16 @@ pub struct BlockSerie {
     transactions_count: u32,
     uncles_count: u32,
     proposals_count: u32,
+    version: u32,
+}
+
+#[derive(InfluxDbWriteable)]
+pub struct UncleSerie {
+    time: Timestamp,
+
+    number: u64,
+    proposals_count: u32,
+    version: u32,
 }
 
 #[derive(InfluxDbWriteable)]
@@ -90,41 +100,84 @@ fn analyze_blocks(query_sender: Sender<WriteQuery>) {
         let tip = rpc.get_tip_block_number();
         (tip.saturating_sub(100000), tip)
     };
-    let mut previous: BlockView = rpc
+    let mut parent: BlockView = rpc
         .get_block_by_number(from.saturating_sub(1))
         .unwrap()
         .into();
     for number in from..=to {
-        if number % 10000 == 0 && number != from {
-            let percent = (to - number + 1) as f64 / (number - from + 1) as f64;
-            let left = start.elapsed().mul_f64(percent);
-            println!(
-                "Getting block #{number}, left {seconds}s ...",
-                number = number,
-                seconds = left.as_secs()
-            );
-        }
+        prompt_progress(to - from + 1, number - from, start);
 
         if let Some(json_block) = rpc.get_block_by_number(number) {
             let block: BlockView = json_block.into();
+            query_sender.send(analyze_block(&block, &parent)).unwrap();
+            analyze_block_uncles(&block).into_iter().for_each(|query| {
+                query_sender.send(query).unwrap();
+            });
 
-            let time = Timestamp::Milliseconds(block.timestamp() as u128);
-            let time_interval = block.timestamp().saturating_sub(previous.timestamp()); // ms
-            let transactions_count = block.transactions().len() as u32;
-            let uncles_count = block.uncles().hashes().len() as u32;
-            let proposals_count = block.union_proposal_ids().len() as u32;
-            let write_query = BlockSerie {
-                time,
-                number,
-                time_interval,
-                transactions_count,
-                uncles_count,
-                proposals_count,
-            }
-            .into_query("blocks");
-            query_sender.send(write_query).unwrap();
-
-            previous = block;
+            parent = block;
         }
+    }
+}
+
+fn analyze_block(block: &BlockView, parent: &BlockView) -> WriteQuery {
+    static QUERY_NAME: &str = "blocks";
+
+    let time = Timestamp::Milliseconds(block.timestamp() as u128);
+    let number = block.number();
+    let time_interval = block.timestamp().saturating_sub(parent.timestamp()); // ms
+    let transactions_count = block.transactions().len() as u32;
+    let uncles_count = block.uncles().hashes().len() as u32;
+    let proposals_count = block.union_proposal_ids().len() as u32;
+    let version = block.version();
+    BlockSerie {
+        time,
+        number,
+        time_interval,
+        transactions_count,
+        uncles_count,
+        proposals_count,
+        version,
+    }
+    .into_query(QUERY_NAME)
+}
+
+fn analyze_block_uncles(block: &BlockView) -> Vec<WriteQuery> {
+    static QUERY_NAME: &str = "uncles";
+
+    let mut queries = Vec::with_capacity(block.uncle_hashes().len());
+    for uncle in block.uncles().data() {
+        let header = uncle.header().into_view();
+        let number = header.number();
+        let proposals_count = uncle.proposals().len() as u32;
+        let time = Timestamp::Milliseconds(header.timestamp() as u128);
+        let version = header.version();
+        let query = UncleSerie {
+            time,
+            number,
+            proposals_count,
+            version,
+        }
+        .into_query(QUERY_NAME);
+        queries.push(query);
+    }
+    queries
+}
+
+fn prompt_progress(total: u64, processed: u64, start: Instant) {
+    const PROMPT_STEP: u64 = 10000;
+
+    if processed % PROMPT_STEP == 0 && processed > 0 {
+        let left = total - processed;
+        let processed_duration = start.elapsed();
+        let left_duration = processed_duration
+            .mul_f64(left as f64)
+            .div_f64(processed as f64);
+        let processed_percent = processed as f64 / total as f64;
+
+        println!(
+            "Progress {:.2}, left {}s ...",
+            processed_percent,
+            left_duration.as_secs()
+        );
     }
 }
