2607c4befbd6aa923feda65ede46fd801ef1a9cc
keroro <keroroxx520@gmail.com>
2020-11-19 16:23:48 +0800

    rename testnet/mainnet to aggron/lina

--- src/app_config.rs
+++ src/app_config.rs
@@ -3,12 +3,12 @@ use ckb_app_config::CKBAppConfig;
 
 pub(crate) fn app_config() -> CKBAppConfig {
     match CONFIG.network.ckb_app_config.as_str() {
-        "ckb.mainnet.toml" => {
-            let bytes = include_bytes!("../specs/ckb.mainnet.toml");
+        "lina" => {
+            let bytes = include_bytes!("../ckb_app_config/lina.toml");
             toml::from_slice(bytes).unwrap()
         }
-        "ckb.testnet.toml" => {
-            let bytes = include_bytes!("../specs/ckb.testnet.toml");
+        "aggron" => {
+            let bytes = include_bytes!("../ckb_app_config/aggron.toml");
             toml::from_slice(bytes).unwrap()
         }
         _ => unimplemented!(),
