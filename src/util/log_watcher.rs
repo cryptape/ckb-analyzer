5175a41eae3f2ab6d46fa8435abc76992f6fcb29
keroro <keroroxx520@gmail.com>
2021-01-06 15:00:17 +0800

    feat(util): add log_watcher

---
+++ src/util/log_watcher.rs
@@ -0,0 +1,94 @@
+use std::fs::Metadata;
+use std::os::macos::fs::MetadataExt;
+use std::path::Path;
+use tokio::fs::File;
+use tokio::io::{AsyncBufReadExt, BufReader, SeekFrom};
+use tokio::time::{delay_for, Duration};
+
+/// `LogWatcher` is a Rust Stream which behaviours like `tail`.
+///
+/// `LogWatcher` accepts parameter `seek: enum { SeekToStart, SeekToEnd }`,
+/// to support the start position. By default `seek = SeekToEnd`.
+///
+/// On polling, `LogWatcher` reopen if the file rotated when trying
+/// to read line at the last recorded position; otherwise returns the
+/// new line.
+pub struct LogWatcher {
+    filepath: String,
+    // file: File,
+    reader: BufReader<File>,
+    metadata: Metadata,
+    // pos: u64,
+}
+
+impl LogWatcher {
+    pub async fn new(filepath: impl AsRef<Path>) -> Self {
+        let filepath = filepath.as_ref().to_string_lossy().to_string();
+        let (mut file, metadata) = Self::open(&filepath).await;
+        let pos = metadata.len();
+        file.seek(SeekFrom::Start(pos)).await.unwrap();
+        let reader = BufReader::new(file);
+        Self {
+            filepath,
+            reader,
+            metadata,
+        }
+    }
+
+    pub async fn watch<Callback: ?Sized>(&mut self, callback: &mut Callback)
+    where
+        Callback: FnMut(String),
+    {
+        let mut line = String::new();
+        loop {
+            line.clear();
+            match self.reader.read_line(&mut line).await {
+                Ok(0) => {
+                    // EOF, try reopen
+                    self.reopen_if_log_rotated().await;
+                }
+
+                Ok(_num_bytes) => {
+                    callback(line.replace("\n", ""));
+
+                    // self.pos += num_bytes as u64;
+                    // self.file.seek(SeekFrom::Start(self.pos));
+                }
+
+                Err(err) => log::error!("read {}, error: {}", self.filepath, err),
+            }
+        }
+    }
+
+    async fn reopen_if_log_rotated(&mut self) {
+        let (file, metadata) = Self::open(&self.filepath).await;
+        if metadata.st_ino() != self.metadata.st_ino() {
+            self.reader = BufReader::new(file);
+            self.metadata = metadata;
+            // self.pos = self.metadata.len();
+            // self.file.seek(SeekFrom::Start(0));
+        }
+    }
+
+    async fn open(filepath: &str) -> (File, Metadata) {
+        loop {
+            let file = match File::open(filepath).await {
+                Ok(file) => file,
+                Err(err) => {
+                    log::error!("open {}, error: {}", filepath, err);
+                    delay_for(Duration::from_secs(1)).await;
+                    continue;
+                }
+            };
+            let metadata = match file.metadata().await {
+                Ok(metadata) => metadata,
+                Err(err) => {
+                    log::error!("read file metadata {}, error: {}", filepath, err);
+                    delay_for(Duration::from_secs(1)).await;
+                    continue;
+                }
+            };
+            return (file, metadata);
+        }
+    }
+}
---
+++ src/util/mod.rs
@@ -0,0 +1,3 @@
+pub mod log_watcher;
+
+pub use log_watcher::LogWatcher;
